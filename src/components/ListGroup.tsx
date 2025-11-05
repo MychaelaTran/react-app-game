6ea110ace2aae77b41ed5aa1d2b9f4c82dd4273a
Mychaela Tran <tranmychaela@gmail.com>
2025-11-05 13:58:13 -0500

    list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,15 @@
 
 function ListGroup(){
+    const items = [ "Ed Sheeran" ,"Conan Gray", "Shawn Mendes", "Aidan Bisset", "Taylor Swift"];
+    
     return (
         <>
             <h1>List</h1>
             <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
-                </ul>
+                {items.map(item => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
         </>
     );
     
